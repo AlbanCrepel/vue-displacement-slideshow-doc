5e92d1f0f80e8559b2e9b45224e978a13e46165c
Emilien <admin@emilienleroy.fr>
2020-06-03 22:18:05 +0200

    :pencil: Add guide and api page

---
+++ .vitepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'vue-displacement-slideshow',
+    description: 'A Vue.js 2.0 slideshow component working with Three.js',
+    themeConfig: {
+        nav: [
+          { text: 'Guide', link: '/' },
+          { text: 'API', link: '/api' },
+        ]
+    }
+  }
--- index.md
+++ index.md
@@ -1,3 +1,3 @@
-# vue-displacement-slideshow
+# Guide
 
-
+## Getting Started
